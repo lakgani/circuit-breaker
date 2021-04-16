6521757c276a3ab522131f4198f2afbda664b79e
Ganesh Pendyala <lak.gani@gmail.com>
2021-04-16 15:29:55 +0530

    Fixed timer in action

--- action.js
+++ action.js
@@ -1,13 +1,13 @@
-function action() {
+async function action() {
   const randVal = Math.floor(Math.random() * 10);
   return new Promise((res, rej) => {
     setTimeout(() => {
       if (randVal % 2 === 0) {
         res("success");
       } else {
-        rej(`Failed due to randVal ${randVal}`);
+        rej(new Error(`Failed due to randVal ${randVal}`));
       }
-    }, randVal);
+    }, randVal * 1000);
   });
 }
 
