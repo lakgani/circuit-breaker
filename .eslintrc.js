aec857c2619433a9c98e3e4ee4fba0f3f06027f9
Ganesh Pendyala <lak.gani@gmail.com>
2021-04-16 23:50:43 +0530

    Add Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,13 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    "jest/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # circuit-breaker
 
 Circuit Breaker implementation
+
+## Steps to run
+* `npm i`
+* `npm run load-test`
